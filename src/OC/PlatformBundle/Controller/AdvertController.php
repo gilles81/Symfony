c97fdc08909cffb0c5e50526dca66f68f50ab8a0
gilles81 <gilles.sabathe@free.fr>
2018-11-13 11:00:17 +0100

    request funxction

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -5,24 +5,27 @@
 namespace OC\PlatformBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 class AdvertController extends Controller
 {
   // La route fait appel à OCPlatformBundle:Advert:view,
   // on doit donc définir la méthode viewAction.
   // On donne à cette méthode l'argument $id, pour
   // correspondre au paramètre {id} de la route
-  public function viewAction($id)
+  // On injecte la requête dans les arguments de la méthode
+
+
+  public function viewAction($id, Request $request)
   {
-    // $id vaut 5 si l'on a appelé l'URL /platform/advert/5
+    // On récupère notre paramètre tag
+    $tag = $request->query->get('tag');
 
-    // Ici, on récupèrera depuis la base de données
-    // l'annonce correspondant à l'id $id.
-    // Puis on passera l'annonce à la vue pour
-    // qu'elle puisse l'afficher
-
-    return new Response("Affichage de l'annonce d'id : ".$id);
+    return $this->render('OCPlatformBundle:Advert:view.html.twig', array(
+      'id'  => $id,
+      'tag' => $tag,
+    ));
   }
 
 
